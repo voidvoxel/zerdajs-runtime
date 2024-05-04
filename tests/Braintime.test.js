5fd3c427f6c8de08155b3a4b67e5116bef023af1
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-05-04 18:44:15 -0400

    Update Braintime.test.js

--- tests/Braintime.test.js
+++ tests/Braintime.test.js
@@ -4,13 +4,22 @@ import { Braintime } from "../src/index.mjs";
 test(
     "initialize braintime",
     async () => {
+        const exampleDir = "examples/xor";
+
         const braintime = new Braintime(
             {
-                cwd: "tmp"
+                cwd: exampleDir
             }
         );
 
-        await braintime.evalModule("examples/xor");
+        const xor = await braintime.evalModule(exampleDir);
+
+        expect(typeof xor).toBe('function');
+
+        expect(xor(0, 0)).toBe(0);
+        expect(xor(0, 1)).toBe(1);
+        expect(xor(1, 0)).toBe(1);
+        expect(xor(1, 1)).toBe(0);
     },
     30000
 );
